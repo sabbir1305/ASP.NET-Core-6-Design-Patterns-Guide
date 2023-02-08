fe93787c07e03c697e3c004b35f186ba59962de2
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2023-02-08 22:11:45 +0600

    Template pattern

---
+++ TemplateSearch/SearchTemplate/LinearSearchMachine.cs
@@ -0,0 +1,20 @@
+﻿namespace TemplateSearch.SearchTemplate
+{
+    public class LinearSearchMachine : SearchMachine
+    {
+        public LinearSearchMachine(params int[] values) : base(values)
+        {
+
+        }
+        protected override int? Find(int value)
+        {
+            int index = 0;
+            foreach (var item in Values)
+            {
+                if (item == value) return index;
+                index++;
+            }
+            return null;
+        }
+    }
+}
